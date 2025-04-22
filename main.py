948af0d8547fe6178a28197252e7b375252c27a0
Laboratorio_BG <Laboratorio_BG@labgrajales.aiep.corp>
2025-04-22 17:02:24 -0400

    cambios realizados

--- main.py
+++ main.py
@@ -1,40 +1,60 @@
+import requests
+
 from dotenv import load_dotenv
+
 import os
-import logging
-from typing import List, Dict, Optional
-
-#----configurar logging---
-logging.basicConfig(
-     level=logging.INFO,
-     format='%(asctime)s- %(levelname)s-%(message)s')
-
-
-
-#---cargar las variables del entorno---
-def load_env()->Optional[Dict[str,str]]:
-    load_dotenv()
-    api_key=os.getenv("API_KEY")
-    search_engine_id=os.getenv("SEARCH_ENGINE_ID")
-
-    if not api_key or not search_engine_id:
-         logging.error("api key o search engine id no estan cargados")
-         return None
-    return{
-         'api_key':api_key,
-         'search_engine_id':search_engine_id
-                     }
-
-def main ():
-     env_vars=load_env()
-     logging.info(f"varianles cargadas{env_vars}")
-     
-if __name__=="__main__":
-     main()
 
 
 
 load_dotenv()
-api_key=os.getenv("API_KEY")
-logging.info("api key cargada correctamente")
+
+API_KEY = os.getenv("API_KEY_SEARCH_GOOGLE")
+
+SEARCH_ENGINE_ID = os.getenv("SEARCH_ENGINE_ID")
 
 
+# configuracion de la consulta y los parametros de busqueda
+
+query = 'filetype:sql "MySQL dump" (pass|password|passwd|pwd)'
+
+page = 1
+
+lang = "lang_es"
+
+# Construcción de la URL para la API de Google Custom Search
+
+url = f"https://www.googleapis.com/customsearch/v1?key={API_KEY}&cx={SEARCH_ENGINE_ID}&q={query}&start={page}&lr={lang}"
+
+# Realizar la solicitud a la API
+
+response = requests.get(url)
+
+data = response.json()
+
+
+
+result= data.get('items', [])
+
+if not result:
+
+    print("No se encontraron resultados.")
+
+else:
+
+    for item in result:
+
+        title = item.get('title')
+
+        link = item.get('link')
+
+        snippet = item.get('snippet')
+
+        print(f"Title: {title}")
+
+        print(f"Link: {link}")
+
+        print(f"Snippet: {snippet}")
+
+        print("-" * 80)
+
+        
